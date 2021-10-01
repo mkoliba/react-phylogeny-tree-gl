87e02cab26a2f0e629394ae5a4bb63476242ed4a
Martin Koliba <mkoliba@users.noreply.github.com>
2021-10-01 14:17:55 +0200

    fix: do not call setRoot when node is leaf

--- src/plugins/createPluginOnViewSubtree.ts
+++ src/plugins/createPluginOnViewSubtree.ts
@@ -1,15 +1,17 @@
-import { Phylocanvas, Decorate } from '../types/phylogeny-tree';
+import { Phylocanvas, Decorate, TreeNode } from '../types/phylogeny-tree';
 
 export function createOnViewSubtreePlugin(
   onViewSubtree: (tree: Phylocanvas, leafsInSubtree: string[]) => void
 ) {
   return function onViewSubtreePlugin(tree: Phylocanvas, decorate: Decorate) {
-    decorate('setRoot', (delegate, args) => {
-      delegate(...args);
-
-      if (tree) {
-        const leafs = tree.getGraphAfterLayout().leaves.map((value) => value.id);
-        onViewSubtree(tree, leafs);
+    decorate('setRoot', (delegate, args: [TreeNode]) => {
+      const [node] = args;
+      if (!node.isLeaf) {
+        delegate(...args);
+        if (tree) {
+          const leafs = tree.getGraphAfterLayout().leaves.map((value) => value.label || value.id);
+          onViewSubtree(tree, leafs);
+        }
       }
     });
   };
