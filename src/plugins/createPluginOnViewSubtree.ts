d7a3f9d7ea6076e5ca8c643f9221dc0aaa9aa2e5
Martin Koliba <mkoliba@users.noreply.github.com>
2022-01-26 17:40:51 +0100

    fixed issue when code in setRoot arg is null

--- src/plugins/createPluginOnViewSubtree.ts
+++ src/plugins/createPluginOnViewSubtree.ts
@@ -1,12 +1,13 @@
-import { Phylocanvas, Decorate, TreeNode } from '../types/phylocanvas.gl';
+import { Phylocanvas, Decorate } from '../types/phylocanvas.gl';
 
 export function createOnViewSubtreePlugin(
   onViewSubtree: (tree: Phylocanvas, leafsInSubtree: string[]) => void
 ) {
   return function onViewSubtreePlugin(tree: Phylocanvas, decorate: Decorate) {
-    decorate('setRoot', (delegate, args: [TreeNode]) => {
-      const [node] = args;
-      if (!node.isLeaf) {
+    decorate('setRoot', (delegate, args) => {
+      const [nodeOrId] = args;
+      const node = this.findNodeById(nodeOrId);
+      if (node === null || node === undefined || !node.isLeaf) {
         delegate(...args);
         if (tree) {
           const leafs = tree.getGraphAfterLayout().leaves.map((value) => value.label || value.id);
